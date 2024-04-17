90aad20a9a616aab9ec9f1305de94d0fddb3e3ae
Mauro124 <maurobeltrame12@gmail.com>
2024-04-16 21:26:49 -0300

    Update API client provider to use different base URLs for debug and release modes

--- lib/infraestructure/api_client_provider.dart
+++ lib/infraestructure/api_client_provider.dart
@@ -1,6 +1,10 @@
 import 'package:control_stock_web_admin/infraestructure/api_client.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final apiClientProvider = Provider<APIClient>((ref) {
-  return APIClient(baseUrl: "http://127.0.0.1:5001/control-stock-2e370/us-central1/api/");
+  return APIClient(
+      baseUrl: kDebugMode
+          ? "http://127.0.0.1:5001/control-stock-2e370/us-central1/api/"
+          : "https://us-central1-control-stock-2e370.cloudfunctions.net/api");
 });
