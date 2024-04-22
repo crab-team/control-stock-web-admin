1471725e466c35f6bb82933627c50b153e310e76
Mauro124 <maurobeltrame12@gmail.com>
2024-04-22 19:16:23 -0300

    Refactor version display logic and add package_info_plus dependency

--- lib/presentation/widgets/shared/version_widget.dart
+++ lib/presentation/widgets/shared/version_widget.dart
@@ -1,14 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
-class VersionWidget extends StatelessWidget {
+class VersionWidget extends StatefulWidget {
   const VersionWidget({super.key});
+
+  @override
+  State<VersionWidget> createState() => _VersionWidgetState();
+}
+
+class _VersionWidgetState extends State<VersionWidget> {
+  String version = '-';
+
+  @override
+  void initState() {
+    super.initState();
+    Future.microtask(() async {
+      PackageInfo platform = await PackageInfo.fromPlatform();
+      setState(() {
+        version = platform.version;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         Text(
-          'v0.1.3',
+          'Version: $version',
           style: Theme.of(context).textTheme.bodyMedium!.copyWith(fontWeight: FontWeight.bold),
         ),
       ],
